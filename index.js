434ad108195ba039b319d653fa904be67893469c
Vinesh-Sivasubramani <91374603+Vinesh-Sivasubramani@users.noreply.github.com>
2023-11-26 19:31:29 +0530

    Update date format error

--- index.js
+++ index.js
@@ -1,97 +1,76 @@
 const express = require("express");
-const app=express();
-const mongoose= require("mongoose");
+const app = express();
+const mongoose = require("mongoose");
 require("dotenv").config();
-app.use(express.json())
+app.use(express.json());
 
 const cors = require("cors");
 app.use(cors({
-    origin:"*"
-}))
+    origin: "*"
+}));
 
-const slots= require("./Slots");
+const slots = require("./Slots");
 
+main().catch((e) => {
+    console.log(e);
+});
 
-main().catch((e)=>{
-    console.log(e)
-})
-
-async function main()
-{
+async function main() {
     await mongoose.connect(process.env.MONGO_URL);
-    
-    // To Create Parking Slots 
+        // To Create Parking Slots 
     // const slot= await slots.create({slot_no:10,slot_status:0})
-    
 }
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendStatus(200);
-})
+});
 
-app.post("/getData", async(req, res) => {
+app.post("/getData", async (req, res) => {
     try {
         const data = req.body.uplink_message.decoded_payload;
-        const {slot_no,slot_status}=data;
-        console.log(slot_no,slot_status);
+        const { slot_no, slot_status } = data;
+        console.log(slot_no, slot_status);
 
-        const slot = await slots.findOne({slot_no:Number(slot_no)});
-        if(!slot){
+        const slot = await slots.findOne({ slot_no: Number(slot_no) });
+        if (!slot) {
             return res.sendStatus(404);
         }
 
-        slot.slot_status=Number(slot_status);
+        slot.slot_status = Number(slot_status);
         const date = new Date();
-        slot.updatedAt=date;
-        await slot.save()
+        slot.updatedAt = date;
+        await slot.save();
         res.status(200).json(data);
-    } 
-    catch (error) 
-    {
+    } catch (error) {
         console.error("Error parsing JSON data:", error);
         res.status(400).json({ error: "Invalid JSON data" });
     }
 });
 
-
-app.get("/api/v1/allData",async(req,res)=>{
+app.get("/api/v1/allData", async (req, res) => {
     const allSlotStatus = await slots.find();
-    res.json(allSlotStatus)
-})
+    res.json(allSlotStatus);
+});
 
-app.post("/api/v1/query",async(req,res)=>{
-    const {slot_status,slot_no}=req.query;
-   
-    const slot = await slots.findOne({slot_no:Number(slot_no)})
-    
-    if(!slot){
+app.post("/api/v1/query", async (req, res) => {
+    const { slot_status, slot_no } = req.query;
+
+    const slot = await slots.findOne({ slot_no: Number(slot_no) });
+
+    if (!slot) {
         return res.sendStatus(404);
     }
-    console.log(slot_status,slot_no);
-    slot.slot_status=Number(slot_status);
+    console.log(slot_status, slot_no);
+    slot.slot_status = Number(slot_status);
 
-    //DATE
-const date = new Date();
+    const date = new Date();
+    const formattedDate = date.toISOString(); 
 
-const options = {
-  timeZone: 'Asia/Kolkata',
-  year: 'numeric',
-  month: '2-digit',
-  day: '2-digit',
-  hour: '2-digit',
-  minute: '2-digit',
-  second: '2-digit',
-};
-
-const formattedDate = new Intl.DateTimeFormat('en-IN', options).format(date);
-
-    slot.updatedAt=formattedDate;
-    await slot.save()
+    slot.updatedAt = formattedDate;
+    await slot.save();
     return res.sendStatus(200);
+});
 
-})
-
-
-app.listen(process.env.PORT||5000,()=>{
+app.listen(process.env.PORT || 5000, () => {
     console.log("Listening🚗🚗🚗");
-})
+});
